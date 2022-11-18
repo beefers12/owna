46c76cddb2fcb083159ce38ad0ed9b4b1a56d947
exeusers <118548468+exeusers@users.noreply.github.com>
2022-11-18 12:34:42 -0300

    Add files via upload

---
+++ ownaa.js
@@ -0,0 +1,6 @@
+{
+    "username": "owned 731",
+    "href": "https://discord.gg/XY9YthYr",
+    "avatar_url": "https://i.pinimg.com/236x/60/0f/23/600f23a36e5bb58272140116de02d860.jpg",
+    "credits": "owned"
+}
